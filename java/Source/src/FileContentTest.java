aab9306320d9b15c9d1f7bb25cb756ed628b01af
Aishik Ghosh <aishik.ghosh@outlook.com>
2018-03-06 03:12:30 -0600

    Finished the Java part of Code. Will now add comments and make some tests

---
+++ java/Source/src/FileContentTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FileContentTest {
+
+    @Test
+    public void makeApiRequest() {
+    }
+
+    @Test
+    public void getFileContentsAsString() {
+    }
+}
