8f50e954a27acdf71f40824d9a36f49e4a3b8b5e
Aishik Ghosh <aishik.ghosh@outlook.com>
2018-03-07 00:09:10 -0600

    Update FrequencyCounterTest.java
    My jdk died, so I am adding comments and making changes through github

--- java/Source/src/FrequencyCounterTest.java
+++ java/Source/src/FrequencyCounterTest.java
@@ -6,12 +6,7 @@ import static org.junit.Assert.*;
 
 public class FrequencyCounterTest {
 
-    @org.junit.Before
-    public void setUp() throws Exception {
-
-    }
-
-
+   
     @Test
     public void removeCommaFullstop() {
         ArrayList<String> testArray = new ArrayList<String>();
@@ -21,12 +16,11 @@ public class FrequencyCounterTest {
 
         assertEquals("lol", FrequencyCounter.removeCommaFullstop(testArray).get(0));
     }
-
+    //trying different order
     @Test
     public void removeCommaFullstop1() {
         ArrayList<String> testArray = new ArrayList<String>();
         testArray.add(",");
-
         testArray.add("word");
         testArray.add(".");
 
@@ -38,13 +32,13 @@ public class FrequencyCounterTest {
         String test = "I i a b c d e f g h i j k l";
         assertEquals("[i=3, a=1, b=1, c=1, d=1, e=1, f=1, g=1, h=1, j=1, k=1, l=1]", FrequencyCounter.main(test).toString());
     }
-
+    //trying with fullstop
     @Test
     public void main2() {
         String test = "Hello. My name is Aishik. Aishik is Aishik and Aishik should be the most common word";
         assertEquals("[aishik=4, is=2, the=1, most=1, be=1, common=1, and=1, name=1, should=1, hello=1, my=1, word=1]", FrequencyCounter.main(test).toString());
     }
-
+    //trying less than 10 words
     @Test
     public void main3() {
         String test = "Hello. My name is Aishik.";
