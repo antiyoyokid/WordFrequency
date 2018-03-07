861ce373d28f2b6ac8ef960f79f72d0e989f6385
Aishik Ghosh <aishik.ghosh@outlook.com>
2018-03-07 16:59:55 -0600

    Update FrequencyCounter.java

--- java/Source/src/FrequencyCounter.java
+++ java/Source/src/FrequencyCounter.java
@@ -7,7 +7,7 @@ import java.util.*;
 
 public class FrequencyCounter {
 
-    private int NUM_OF_WORDS =10; //number of words, required for the frequency
+    private static int NUM_OF_WORDS = 10; //number of words, required for the frequency
 
     /**
      * Removes invalid characters
@@ -15,7 +15,7 @@ public class FrequencyCounter {
      * @param allWords Arraylist of all the words
      * @return allWords without any commas and fullstops
      */
-    public static ArrayList<String> removeCommaFullstop(ArrayList<String> allWords) {
+    public ArrayList<String> removeCommaFullstop(ArrayList<String> allWords) {
         // Removes any commas or fullstops
         for (int j = 0; j < allWords.size(); j++) {
             if (allWords.get(j).contains(",")) {
@@ -28,35 +28,52 @@ public class FrequencyCounter {
         return allWords;
     }
 
-
     /**
-     * Counts the frequency of certain words
-     *
-     * @param text to find the most common words in
-     * @return List containing
-     * I could have broken this down into 3 more methods, but it didn't seem necessary as the methods were all
-     * very simple and easy to follow and breaking them down would only make it harder for the reader to follow.
+     * @param text
+     * @return ArrayList
      */
-    public static List<HashMap.Entry<String, Integer>> frequencyCounter(String text) {
 
+    public ArrayList<String> convertTextToArrayList(String text) {
         // Creates an array of the words without any special characters
         String[] words = text.toLowerCase().split("(?=[,.])|\\s+");
         ArrayList<String> wordsToArrayList = new ArrayList<String>(Arrays.asList(words));
         wordsToArrayList = removeCommaFullstop(wordsToArrayList);
+        return wordsToArrayList;
+    }
 
-
-        // Creates a hashmap with each word mapped to a value of its frequency
+    /**
+     *
+     * @param text
+     * @return hasmap with the word and its frequency
+     */
+    public HashMap<String, Integer> updateWordAndFrequency(ArrayList<String> text) {
         HashMap<String, Integer> allWords = new HashMap<String, Integer>();
-        for (String word : wordsToArrayList) {
+        for (String word : text) {
             if (allWords.containsKey(word)) {
                 allWords.put(word, allWords.get(word) + 1); //if the word exists add one to value
             } else {
                 allWords.put(word, 1); //otherwise put the word into the hasmap and set value to 1
             }
         }
+        return allWords;
+    }
+
+    /**
+     * Counts the frequency of certain words
+     *
+     * @param text
+     * @return List containing the words in order
+     */
+    public List<HashMap.Entry<String, Integer>> frequencyCounter(String text) {
+
+        ArrayList<String> textoArrayList;
+        textoArrayList = convertTextToArrayList(text);
+
+        HashMap<String, Integer> wordAndFrequency;
+        wordAndFrequency = updateWordAndFrequency(textoArrayList);
 
         // Creates a Set of the hashmap and sorts it
-        Set<HashMap.Entry<String, Integer>> entrySet = allWords.entrySet();
+        Set<HashMap.Entry<String, Integer>> entrySet = wordAndFrequency.entrySet();
         List<HashMap.Entry<String, Integer>> list = new ArrayList<Map.Entry<String, Integer>>(entrySet);
         //http://www.speakingcs.com/2015/06/how-to-sort-hashmap-based-on-values-in.html
         Collections.sort(list, new Comparator<HashMap.Entry<String, Integer>>() {
@@ -77,12 +94,12 @@ public class FrequencyCounter {
      * @throws UnirestException
      */
     public static void main(String[] args) throws IOException, UnirestException {
-
+        FrequencyCounter wordFrequency = new FrequencyCounter();
         String url = UserInput.website();
         String text = FileContent.makeApiRequest(url); // returns as String the entire text
 
         for (int i = 0; i < NUM_OF_WORDS; i++) {
-            System.out.println(frequencyCounter(text).get(i));
+            System.out.println(wordFrequency.frequencyCounter(text).get(i));
         }
     }
 }
