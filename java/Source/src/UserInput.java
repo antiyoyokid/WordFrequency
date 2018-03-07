7bc5cfcd6adfce8306fa04c1dbcfd34d4ff5ca04
Aishik Ghosh <aishik.ghosh@outlook.com>
2018-03-07 00:11:56 -0600

    Update UserInput.java

--- java/Source/src/UserInput.java
+++ java/Source/src/UserInput.java
@@ -2,7 +2,10 @@ import java.util.Scanner;
 
 public class UserInput {
 
-
+    /**
+     * Promps reader for url
+     * @return the url user entered
+     */
     public static String website() {
         System.out.println("Enter a URL");
         Scanner input = new Scanner(System.in);
